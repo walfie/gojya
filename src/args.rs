718c53dee06da378e7db372712c6d4cca4961e10
Walfie <walfington@gmail.com>
2017-08-18 16:50:49 -0400

    Modify argument parsing

--- src/args.rs
+++ src/args.rs
@@ -1,37 +1,64 @@
 use error::*;
-use olifants;
+use olifants::timeline::Endpoint;
+use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-pub struct Opt {
+struct Opt {
     #[structopt(short = "i", long = "instance",
-                help = "Instance URL (HTTPS is assumed if protocol is unspecified)")]
+                help = "Instance URL (if protocol is absent, `https` is assumed)")]
     pub instance: String,
 
-    #[structopt(short = "t", long = "token", help = "Access token")]
-    pub access_token: String,
+    #[structopt(short = "t", long = "token",
+                help = "Access token. If unspecified, uses the \
+                        `MASTODON_ACCESS_TOKEN` environment variable.")]
+    pub token: Option<String>,
 
-    #[structopt(help = "Stream type")]
-    pub stream_type: StreamType,
+    #[structopt(long = "timeline", help = "Timeline type", default_value = "local",
+                possible_value = "local", possible_value = "federated", possible_value = "user")]
+    pub timeline: String,
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct StreamType(pub olifants::timeline::Endpoint);
+pub struct Args {
+    pub instance_url: String,
+    pub access_token: String,
+    pub endpoint: Endpoint,
+}
 
-impl ::std::str::FromStr for StreamType {
-    type Err = Error;
+impl Args {
+    pub fn init() -> Result<Self> {
+        let args = Opt::from_args();
 
-    fn from_str(s: &str) -> Result<Self> {
+        // If no protocol specified, assume `https://`
+        let instance_url =
+            if args.instance.starts_with("http://") || args.instance.starts_with("https://") {
+                args.instance
+            } else {
+                format!("https://{}", args.instance)
+            };
+
+        let access_token = args.token
+            .or_else(|| ::std::env::var("MASTODON_ACCESS_TOKEN").ok())
+            .ok_or_else(|| {
+                let msg = "please specify an access token with `--token`, or by \
+                       setting the `MASTODON_ACCESS_TOKEN` environment variable";
+                Error::from_kind(ErrorKind::Msg(msg.into()))
+            })?;
+
         use olifants::timeline::Endpoint::*;
 
-        let result = match s {
+        let endpoint = match args.timeline.as_ref() {
             "user" => User,
             "notification" => Notification,
             "notifications" => Notification,
             "federated" => Federated,
             "local" => Local,
-            other => Other(other.to_string()), // TODO: Error
+            other => Other(other.to_string()), // TODO: Error?
         };
 
-        Ok(StreamType(result))
+        Ok(Args {
+            instance_url,
+            access_token,
+            endpoint,
+        })
     }
 }
